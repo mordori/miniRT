dc85424a6c8b42d5c986baa5c4e1888855b19b89
wshoweky <wshoweky@student.hive.fi>
2025-12-17 19:34:37 +0200

    Adding more parsing tools

---
+++ src/parsing/parse_tools.c
@@ -0,0 +1,83 @@
+#include "parsing.h"
+#include "libft_io.h"
+#include "libft_str.h"
+#include "utils.h"
+#include <stdlib.h>
+#include <math.h>
+
+bool parse_float(char *str, float *out)
+{
+    char *end;
+    float value;
+
+    if (!str || !out)
+        return (false);
+
+    while (ft_isspace(*str))
+        str++;
+    if (*str == '\0')
+        return (false);
+    value = ft_atof(str, &end);
+    if (end == str)
+        return (false);
+    while (ft_isspace(*end))
+        end++;
+    if (*end != '\0')
+        return (false);
+    if (isnan(value) || isinf(value)) // check for NaN or Infinity
+        return (false);
+    *out = value;
+    return (true);
+}
+
+bool parse_color(char *str, t_color *color)
+{
+    char **components;
+    float r;
+    float g;
+    float b;
+    bool ret;
+
+    if (!str || !color)
+        return (false);
+    components = ft_split(str, ',');
+    ret = false;
+    if (!components)
+        return (false);
+    if (count_tokens(components) == 3)
+    {
+        if (parse_float(components[0], &r)
+            && parse_float(components[1], &g)
+            && parse_float(components[2], &b))
+        {
+            if (r >= 0 && r <= 255 && g >= 0 && g <= 255 && b >= 0 && b <= 255)
+            {
+                color->r = r / 255.0f;
+                color->g = g / 255.0f;
+                color->b = b / 255.0f;
+                // color->a = 255; // fully opaque
+                ret = true;
+            }
+        }
+    }
+    free_tokens(components);
+    return (ret);
+}
+
+int count_tokens(char **tokens)
+{
+    int count;
+
+    if (!tokens)
+        return (0);
+    count = 0;
+    while (tokens[count])
+        count++;
+    return (count);
+}
+
+// a wrapper to free the tokens array
+void free_tokens(char **tokens)
+{
+    ft_free_split(tokens);
+}
