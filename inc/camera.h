cc35bcdab2f003483dd67a00e05690d763922651
wshoweky <wshoweky@student.hive.fi>
2026-02-17 22:09:55 +0200

    added camera movement and parsing layers

--- inc/camera.h
+++ inc/camera.h
@@ -1,10 +1,12 @@
-#ifndef MINIRT_CAMERA_H
-# define MINIRT_CAMERA_H
+#ifndef CAMERA_H
+# define CAMERA_H
 
 # include "defines.h"
 
-void	init_camera(t_context *ctx, t_vec3 position, t_vec3 orientation, float fov);
+void	init_camera(t_context *ctx, t_vec3 position, t_vec3 orientation,
+			float fov);
 void	update_camera(t_context *ctx, t_camera *cam);
 bool	control_camera(t_context *ctx);
+bool	camera_movement(t_context *ctx);
 
 #endif
