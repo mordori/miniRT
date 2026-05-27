c2c80f6ff04fc4e97a173fc856321173c2d7b90d
Mika Yli-Pentti <mika.ylipentti@gmail.com>
2026-05-27 13:12:27 +0300

    Formatting

--- src/utils/lib_math/fast_math.c
+++ src/utils/lib_math/fast_math.c
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   fast_math.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: wshoweky <wshoweky@student.hive.fi>        +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/03/10 19:48:25 by wshoweky          #+#    #+#             */
-/*   Updated: 2026/03/10 19:48:27 by wshoweky         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "lib_math.h"
 
 /**
@@ -17,26 +5,19 @@
  * Max error: ~0.005 radians (~0.3 degrees). Invisible in path tracing.
  * Replaces glibc atan2f (~760M Ir savings in spherical_uv).
  */
-float	fast_atan2f(float y, float x)
-{
-	float	abs_x;
-	float	abs_y;
-	float	a;
-	float	s;
-	float	r;
-
-	abs_x = fabsf(x) + 1e-10f;
-	abs_y = fabsf(y);
-	a = fminf(abs_x, abs_y) / fmaxf(abs_x, abs_y);
-	s = a * a;
-	r = ((-0.0464964749f * s + 0.15931422f) * s - 0.327622764f) * s * a + a;
+float fast_atan2f(float y, float x) {
+	float abs_x = fabsf(x) + 1e-10f;
+	float abs_y = fabsf(y);
+	float a = fminf(abs_x, abs_y) / fmaxf(abs_x, abs_y);
+	float s = a * a;
+	float r = ((-0.0464964749f * s + 0.15931422f) * s - 0.327622764f) * s * a + a;
 	if (abs_y > abs_x)
 		r = (float)M_PI_2 - r;
 	if (x < 0.0f)
 		r = (float)M_PI - r;
 	if (y < 0.0f)
 		r = -r;
-	return (r);
+	return r;
 }
 
 /**
@@ -44,20 +25,16 @@ float	fast_atan2f(float y, float x)
  * Max error: ~0.02 radians (~1 degree). Good enough for UV mapping.
  * Replaces glibc acosf (~310M Ir savings in spherical_uv).
  */
-float	fast_acosf(float x)
-{
-	float	negate;
-	float	ret;
-
-	negate = (float)(x < 0.0f);
+float fast_acosf(float x) {
+	float negate = (float)(x < 0.0f);
 	x = fabsf(x);
-	ret = -0.0187293f;
+	float ret = -0.0187293f;
 	ret = ret * x + 0.0742610f;
 	ret = ret * x - 0.2121144f;
 	ret = ret * x + 1.5707288f;
 	ret = ret * sqrtf(1.0f - x);
 	ret = ret - 2.0f * negate * ret;
-	return (negate * (float)M_PI + ret);
+	return negate * (float)M_PI + ret;
 }
 
 /**
@@ -66,16 +43,12 @@ float	fast_acosf(float x)
  * Computed using s * q for a "free" x^0.75 term.
  * Max error: ~0.3%.
  */
-float	fast_srgb(float x)
-{
-	float	s;
-	float	q;
-	float	sq;
-
+float fast_srgb(float x) {
 	if (x <= 0.0f)
-		return (0.0f);
-	s = sqrtf(x);
-	q = sqrtf(s);
-	sq = s * q;
-	return (0.04400f * q + 1.17400f * s - 0.28500f * sq + 0.06700f * x);
+		return 0.0f;
+
+	float s = sqrtf(x);
+	float q = sqrtf(s);
+	float sq = s * q;
+	return 0.04400f * q + 1.17400f * s - 0.28500f * sq + 0.06700f * x;
 }
--- src/utils/lib_math/mat4_transforms_2.c
+++ src/utils/lib_math/mat4_transforms_2.c
@@ -1,24 +1,10 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   mat4_transforms_2.c                                :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: myli-pen <myli-pen@student.hive.fi>        +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/02/01 07:34:25 by myli-pen          #+#    #+#             */
-/*   Updated: 2026/02/06 04:28:08 by myli-pen         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "lib_math.h"
 
-t_mat4	mat4_shear(t_vec3 x, t_vec3 y, t_vec3 z)
-{
-	t_mat4		res;
-
-	res.rows[0] = (t_v4sf){1.0f, x.y, x.z, 0.0f};
-	res.rows[1] = (t_v4sf){y.x, 1.0f, y.z, 0.0f};
-	res.rows[2] = (t_v4sf){z.x, z.y, 1.0f, 0.0f};
-	res.rows[3] = (t_v4sf){0.0f, 0.0f, 0.0f, 1.0f};
-	return (res);
+t_mat4 mat4_shear(t_vec3 x, t_vec3 y, t_vec3 z) {
+	return (t_mat4){ //
+		.rows[0] = (t_v4sf){ 1.0f, x.y, x.z, 0.0f },
+		.rows[1] = (t_v4sf){ y.x, 1.0f, y.z, 0.0f },
+		.rows[2] = (t_v4sf){ z.x, z.y, 1.0f, 0.0f },
+		.rows[3] = (t_v4sf){ 0.0f, 0.0f, 0.0f, 1.0f }
+	};
 }
